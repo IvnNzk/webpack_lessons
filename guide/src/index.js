885a3e392dd69ba6e3fd0800bc8964fb86157279
ivan.nizickiy@gmail.com <ivan.nizickiy@gmail.com>
2018-07-28 17:07:15 +0800

    asset management guide webpack

--- guide/src/index.js
+++ guide/src/index.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import imageHelloFile from './hello2.png';
+
+import dataXML from './data.xml';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  let imageHello = new Image();
+  imageHello.src = imageHelloFile;
+  element.appendChild(imageHello);
+
+  console.log(dataXML);
 
   return element;
 }
